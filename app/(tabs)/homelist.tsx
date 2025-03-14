587e8a66e981f6542838dde2befbb8fdf49d2655
Shaheer <khansh11@msu.edu>
2025-03-14 19:07:36 -0400

    Added component

--- app/(tabs)/homelist.tsx
+++ app/(tabs)/homelist.tsx
@@ -1,11 +1,14 @@
 // app/(tabs)/homelist.tsx
 import { View } from 'react-native';
-import { ThemedText } from '@/components/ThemedText';
+import { Heading } from "@/components/ui/heading"
 
 export default function HomeList() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <ThemedText>Welcome to HomeList!</ThemedText>
+        <View >
+        
+        </View>
+      <Heading size='xl'>Welcome to HomeList!</Heading>
     </View>
   );
 }
